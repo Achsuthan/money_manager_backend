93295c559c4509c8d311be030b95c981a3aebb75
Achsuthan <achsuthan@icloud.com>
2021-09-05 20:52:05 +0800

    Common constant file added

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,19 @@
+package constants;
+
+public class Constants {
+	
+	//Common
+	
+	
+	//Failures
+	public static String failure = "failure";
+	public static String failureMessage = "Data fetched failed";
+	public static String dataMissing = "Some data missing in the body";
+	public static String serverErrorMessage = "Somethig went wrong with server please try again later";
+	public static String pageNotFound = "The page not found";
+	
+	//Success
+	public static String success = "success";
+	public static String successCoomonMessage = "Data fetched succesfully";
+	
+}
