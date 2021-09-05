1b20e347f3b1c8cf5cd021daabc933dc7153c6a8
Achsuthan <achsuthan@icloud.com>
2021-09-05 15:19:12 +0800

    API Reponse handler added

---
+++ src/main/java/unitls/ApiResponseHandler.java
@@ -0,0 +1,74 @@
+package unitls;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ApiResponseHandler {
+	
+	public static String apiResponse(ResponseType responseType)  {
+		try {
+			return getCommonResponse(responseType).toString();
+		}
+		catch(Exception e) {
+			return "";
+		}
+	}
+	
+	public static String apiResponse(ResponseType responseType, String message)  {
+		try {
+			JSONObject response = getCommonResponse(responseType);
+			response.put("message", message);
+			return response.toString();
+		}
+		catch(Exception e) {
+			return "";
+		}
+	}
+	
+	public static String apiResponse(ResponseType responseType, String message, JSONObject bodyJson)  {
+		try {
+			JSONObject response = getCommonResponse(responseType);
+			response.put("message", message);
+			response.putOpt("body", bodyJson);
+			return response.toString();
+		}
+		catch(Exception e) {
+			return "";
+		}
+	}
+	
+	private static JSONObject getCommonResponse(ResponseType responseType) throws JSONException {
+		switch (responseType) {
+		case SUCCESS: {
+			JSONObject json = new JSONObject();
+			json.put("status", Constants.success);
+			json.put("code", 200);
+			json.put("message", Constants.successCoomonMessage);
+			return json;
+		}
+		case FAILURE: {
+			JSONObject json = new JSONObject();
+			json.put("status", Constants.failure);
+			json.put("code", 400);
+			json.put("message", Constants.failureMessage);
+			return json;
+		}
+		case DATAMISSING: {
+			JSONObject json = new JSONObject();
+			json.put("status", Constants.failure);
+			json.put("code", 400);
+			json.put("message", Constants.dataMissing);
+			return json;
+		}
+		case SERVERERROR: {
+			JSONObject json = new JSONObject();
+			json.put("status", Constants.failure);
+			json.put("code", 500);
+			json.put("message", Constants.serverErrorMessage);
+			return json;
+		}
+		}
+		return null;
+
+	}
+}
