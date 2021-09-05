62069a3b5433b35b125fd333677d77a9f6ff4439
Achsuthan <achsuthan@icloud.com>
2021-09-05 15:19:28 +0800

    Basic constraints file added

---
+++ src/main/java/unitls/Constants.java
@@ -0,0 +1,14 @@
+package unitls;
+
+public class Constants {
+	public static String success = "success";
+	public static String failure = "failure";
+	public static String successCoomonMessage = "Data fetched succesfully";
+	public static String failureMessage = "Data fetched failed";
+	public static String dataMissing = "Some data missing in the body";
+	public static String serverErrorMessage = "Somethig went wrong with server please try again later";
+	
+	//Login 
+	public static String emailForatRequired = "Please enter valid email format";
+	public static String loginSuccess = "Login successfully"; 
+}
