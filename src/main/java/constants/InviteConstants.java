a18639af908a7537258003825f7c483a57785794
Achsuthan <achsuthan@icloud.com>
2021-09-10 17:59:13 +0800

    invite class constraints added

---
+++ src/main/java/constants/InviteConstants.java
@@ -0,0 +1,21 @@
+package constants;
+
+public class InviteConstants {
+	
+	//Common 
+	public static String firstInviteId = "INV-11111111";
+	public static Integer linkExpiryDays = 90;
+		
+	//Success 
+	public static String inviteCreatedSuccessfully = "Invite created successfully";
+	
+	//Failure
+	public static String userNotExist = "The user is not exist in the system";
+	public static String EmailAlreadyExist = "Email already exist";
+	public static String inviteCreateFailed = "Something went wrong when create invite";
+	public static String userAlreadyExist = "This user is alredy exist in the sytem";
+	public static String emailFormatRequired = "Please enter valid email format";
+	public static String fieldsMissing = "Please check all the values";
+	
+
+}
