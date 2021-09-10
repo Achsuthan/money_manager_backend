d4c483c38e90598a9007b0d7d0cf5a7bcdaa6e4a
Achsuthan <achsuthan@icloud.com>
2021-09-10 18:01:09 +0800

    some user constraints added

--- src/main/java/constants/UserConstants.java
+++ src/main/java/constants/UserConstants.java
@@ -1,6 +1,7 @@
 package constants;
 
 public class UserConstants {
+	
 	//Common messages
 	public static String emailForatRequired = "Please enter valid email format";
 	public static String loginSuccess = "Login successfully"; 
@@ -10,10 +11,12 @@ public class UserConstants {
 	//Success
 	public static String userRegisterSuccessfully = "User registered successfully";
 	public static String loginSuccessfully = "User loggedin successfully";
+	public static String usersFoundSuccessfully = "Search keyword users found";
 	
 	//Failures
 	public static String sqlRegisterError = "Something went wrond while register user please try again later";
 	public static String emailAlreadyExist = "This email address is already exist please try again with new email";
 	public static String loginFailed = "Please check your email address and password";
+	public static String searchKeywordNotFound = "Search keyword users are not found";
 	
 }
